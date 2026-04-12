45219a988c8533886425c40988f642dc4a8c4d3d
Alexandre Abreu Ferreira <ale7java@gmail.com>
2026-04-12 11:47:55 -0300

    Inserindo login

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from 'react'
+import { useState } from 'react'
+import type { FormEvent } from 'react'
 import { Navigate, useLocation, useNavigate } from 'react-router-dom'
 
 import { setToken, hasAuthToken } from '../auth/auth'
